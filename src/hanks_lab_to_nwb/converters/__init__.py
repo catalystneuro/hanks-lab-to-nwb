1e0fbd71c3fee37531436daed8d287a468711b8e
weiglszonja <wszonja@gmail.com>
2026-07-07 16:52:26 +0200

    Add common converter

---
+++ src/hanks_lab_to_nwb/converters/__init__.py
@@ -0,0 +1,3 @@
+from hanks_lab_to_nwb.converters.nwbconverter import HanksLabNWBConverter
+
+__all__ = ["HanksLabNWBConverter"]
---
+++ src/hanks_lab_to_nwb/converters/nwbconverter.py
@@ -0,0 +1,15 @@
+"""NWBConverter for Hanks lab fiber photometry dataset."""
+from neuroconv import NWBConverter
+from neuroconv.datainterfaces import DoricFiberPhotometryInterface
+
+
+class HanksLabNWBConverter(NWBConverter):
+    """Primary conversion class for the Hanks lab fiber photometry dataset."""
+
+    data_interface_classes = dict(
+        DoricFP=DoricFiberPhotometryInterface,
+    )
+
+    def temporally_align_data_interfaces(self, metadata=None, conversion_options=None):
+        # TODO: implement temporal alignment for the Hanks lab NWBConverter
+        pass
